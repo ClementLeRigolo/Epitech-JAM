804ac9c00889bba6a51465c7354933fec03f8edb
ClementLeRigolo <clement1.thomas@epitech.eu>
2022-05-20 18:37:41 +0200

    Adding first assets

---
+++ include/images.h
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2022
+** Epitech-JAM
+** File description:
+** images
+*/
+
+#pragma once
+
+#define bart "assets/bart.png"
+#define lisa "assets/lisa.png"
+#define homer "assets/homer.png"
+#define marge "assets/marge.png"
+#define leo "assets/leo.png"
